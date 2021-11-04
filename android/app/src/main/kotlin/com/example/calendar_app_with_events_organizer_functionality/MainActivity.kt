4ddb66e9354d81c6f7a963c49a78be9494f1cfe1
innocenty-lab <putraramadhan532@gmail.com>
2021-11-04 17:10:21 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/calendar_app_with_events_organizer_functionality/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calendar_app_with_events_organizer_functionality
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:calendar_app_with_events_organizer_functionality/demo_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: DemoApp(),
+    );
+  }
+}
